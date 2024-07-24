239fc30973f278c390a8057b4a491f1347220f4a
leegerken <lee@leegerken.org>
2024-07-24 00:44:23 -0400

    switched from dialogic to dialogue manager/ fixed pause screen mouse dismiss

--- scripts/sign.gd
+++ scripts/sign.gd
@@ -1,1 +1,8 @@
 extends StaticBody2D
+
+func _input(event):
+	if event.is_action_pressed("confirm"):
+		read_sign()
+
+func read_sign():
+	DialogueManager.show_dialogue_balloon(load("res://scripts/dialog/main.dialogue"),"main")
