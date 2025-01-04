8cc45fa09b3922f5c36766d858b2ae279332fe89
Lee Gerken <lee@leegerken.org>
2025-01-04 04:33:38 -0500

    file cleanup/rearrange

--- scripts/splash_screen.gd
+++ scripts/splash_screen.gd
@@ -2,4 +2,4 @@ extends Control
 
 func _ready() -> void:
 	await $AnimationPlayer.animation_finished
-	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
